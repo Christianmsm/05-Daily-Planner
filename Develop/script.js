d3485929e1ac8d9ca9f4c21b1a2e7550e07ef027
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-02-25 07:25:58 -0500

    added current date and buttons save to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,106 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let currentDayEl = $("#currentDay");
+let saves = 0;
+let pastHours = $(".past");
+let presentHour = $("present");
+let futureHour = $("future");
+let userInput = document.querySelector(".description");
+let saveHour9 = document.querySelector("#hour-9");
+
+let textarea9 = $('#txtarea9');
+let textarea10 = $('#txtarea10');
+let textarea11 = $('#txtarea11');
+let textarea12 = $('#txtarea12');
+let textarea1 = $('#txtarea1');
+let textarea2 = $('#txtarea2');
+let textarea3 = $('#txtarea3');
+let textarea4 = $('#txtarea4');
+let textarea5 = $('#txtarea5');
+
+
+
+function currentDay() {
+  let theDate = dayjs().format('dddd, MMMM D');
+  currentDayEl.text(theDate);
+}
+currentDay();
+
+// saveInput.addEventListener("click", function(event) {
+// event.preventDefault();  
+// })
+
+
+// localStorage.setItem("", "");
+// console.log(localStorage.getItem(""))
+
+// localStorage.clear
+
+// let saveInput = document.querySelector(".saveBtn");
+
+
+$("#btn9").on('click',()=> {
+  console.log('txtarea9', textarea9.val());
+  localStorage.setItem("textarea9", textarea9.val() );
+})
+
+
+$("#btn10").on('click',()=> {
+  console.log('txtarea10', textarea10.val());
+  localStorage.setItem("textarea10", textarea10.val() );
+})
+
+
+$("#btn11").on('click',()=> {
+  console.log('txtarea11', textarea11.val());
+  localStorage.setItem("textarea11", textarea11.val() );
+})
+
+
+$("#btn12").on('click',()=> {
+  console.log('txtarea12', textarea12.val());
+  localStorage.setItem("textarea12", textarea12.val() );
+})
+
+
+$("#btn1").on('click',()=> {
+  console.log('txtarea1', textarea1.val());
+  localStorage.setItem("textarea1", textarea1.val() );
+})
+
+
+$("#btn2").on('click',()=> {
+  console.log('txtarea2', textarea2.val());
+  localStorage.setItem("textarea2", textarea2.val() );
+})
+
+
+$("#btn3").on('click',()=> {
+  console.log('txtarea3', textarea3.val());
+  localStorage.setItem("textarea3", textarea3.val() );
+})
+
+
+$("#btn4").on('click',()=> {
+  console.log('txtarea4', textarea4.val());
+  localStorage.setItem("textarea4", textarea4.val() );
+})
+
+
+$("#btn5").on('click',()=> {
+  console.log('txtarea5', textarea5.val());
+  localStorage.setItem("textarea5", textarea5.val() );
+})
+
+
+// console.log(localStorage.getItem("textarea9"));
+
+
 $(function () {
+
+    
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,6 +117,6 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
 });
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 05-Daily-Planner
 This is the 5th challenge in the bootcamp where we create a daily planner.
+
+
+GIVEN I am using a daily planner to create a schedule
+
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar 
+<!-- complete -->
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+<!-- complete -->
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+WHEN I click into a time block
+THEN I can enter an event
+
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+<!-- completed -->
+WHEN I refresh the page
+THEN the saved events persist
