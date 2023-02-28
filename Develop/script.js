08cfc1b0b8ca7bc5847fae959698ce3b781bb977
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-02-28 12:05:01 -0500

    completed assignment, committing final work

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 let currentDayEl = $("#currentDay");
-let saves = 0;
-let pastHours = $(".past");
-let presentHour = $("present");
-let futureHour = $("future");
-let userInput = document.querySelector(".description");
-let saveHour9 = document.querySelector("#hour-9");
-
 let textarea9 = $('#txtarea9');
 let textarea10 = $('#txtarea10');
 let textarea11 = $('#txtarea11');
@@ -21,102 +11,92 @@ let textarea5 = $('#txtarea5');
 
 
 
-function currentDay() {
-  let theDate = dayjs().format('dddd, MMMM D');
-  currentDayEl.text(theDate);
-}
-currentDay();
-
-// saveInput.addEventListener("click", function(event) {
-// event.preventDefault();  
-// })
-
-
-// localStorage.setItem("", "");
-// console.log(localStorage.getItem(""))
-
-// localStorage.clear
-
-// let saveInput = document.querySelector(".saveBtn");
-
-
-$("#btn9").on('click',()=> {
-  console.log('txtarea9', textarea9.val());
-  localStorage.setItem("textarea9", textarea9.val() );
-})
-
-
-$("#btn10").on('click',()=> {
-  console.log('txtarea10', textarea10.val());
-  localStorage.setItem("textarea10", textarea10.val() );
-})
-
-
-$("#btn11").on('click',()=> {
-  console.log('txtarea11', textarea11.val());
-  localStorage.setItem("textarea11", textarea11.val() );
-})
-
-
-$("#btn12").on('click',()=> {
-  console.log('txtarea12', textarea12.val());
-  localStorage.setItem("textarea12", textarea12.val() );
-})
-
-
-$("#btn1").on('click',()=> {
-  console.log('txtarea1', textarea1.val());
-  localStorage.setItem("textarea1", textarea1.val() );
-})
-
-
-$("#btn2").on('click',()=> {
-  console.log('txtarea2', textarea2.val());
-  localStorage.setItem("textarea2", textarea2.val() );
-})
-
-
-$("#btn3").on('click',()=> {
-  console.log('txtarea3', textarea3.val());
-  localStorage.setItem("textarea3", textarea3.val() );
-})
-
-
-$("#btn4").on('click',()=> {
-  console.log('txtarea4', textarea4.val());
-  localStorage.setItem("textarea4", textarea4.val() );
-})
-
-
-$("#btn5").on('click',()=> {
-  console.log('txtarea5', textarea5.val());
-  localStorage.setItem("textarea5", textarea5.val() );
-})
-
-
-// console.log(localStorage.getItem("textarea9"));
-
 
 $(function () {
 
     
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  function currentDay() {
+    let theDate = dayjs().format('dddd, MMMM D');
+    currentDayEl.text(theDate);
+  }
+  currentDay();
   
-  // TODO: Add code to display the current date in the header of the page.
+  function savedEntry() {
+  
+  $("#btn9").on('click',()=> {
+    console.log('txtarea9', textarea9.val());
+    localStorage.setItem("textarea9", textarea9.val() );
+  })
+  
+  
+  $("#btn10").on('click',()=> {
+    console.log('txtarea10', textarea10.val());
+    localStorage.setItem("textarea10", textarea10.val() );
+  })
+  
+  
+  $("#btn11").on('click',()=> {
+    console.log('txtarea11', textarea11.val());
+    localStorage.setItem("textarea11", textarea11.val() );
+  })
+  
+  
+  $("#btn12").on('click',()=> {
+    console.log('txtarea12', textarea12.val());
+    localStorage.setItem("textarea12", textarea12.val() );
+  })
+  
+  
+  $("#btn1").on('click',()=> {
+    console.log('txtarea1', textarea1.val());
+    localStorage.setItem("textarea1", textarea1.val() );
+  })
+  
+  
+  $("#btn2").on('click',()=> {
+    console.log('txtarea2', textarea2.val());
+    localStorage.setItem("textarea2", textarea2.val() );
+  })
+  
+  
+  $("#btn3").on('click',()=> {
+    console.log('txtarea3', textarea3.val());
+    localStorage.setItem("textarea3", textarea3.val() );
+  })
+  
+  
+  $("#btn4").on('click',()=> {
+    console.log('txtarea4', textarea4.val());
+    localStorage.setItem("textarea4", textarea4.val() );
+  })
+  
+  
+  $("#btn5").on('click',()=> {
+    console.log('txtarea5', textarea5.val());
+    localStorage.setItem("textarea5", textarea5.val() );
+  })
+  
+  }
+  
+  
+  savedEntry();
+  
+  var currentHour = dayjs().hour();
+  newDate = dayjs().hour();
+  function plannerColor() {
+    if (currentHour.isSame(dayjs().hour(9))){
+      hours.nineAm.addClass('.present');
+      hours.nineAm.remove('.past');
+    }
+    else if (currentHour.isAfter(dayjs().hour(9))){
+      hours.nineAm.addClass('.past');
+      hours.nineAm.remove('.present');
+    }
+    else if (currentHour.isBefore(dayjs().hour(9))){
+      hours.nineAm.addClass('.future');
+      hours.nineAm.remove('.past');
+    }
+   }
+   
+  console.log(currentHour);
 });
--- README.md
+++ README.md
@@ -1,23 +1,61 @@
 # 05-Daily-Planner
-This is the 5th challenge in the bootcamp where we create a daily planner.
+
+## Description
+
+This project we were tasked with creating a daily planner application that the user can add tasks in the text fields that will then be saved and stored in the local storage. 
+
+How I went about completting this task was I worked in the HTML file to get the header element aligned in the center, then I created all of the div elements for the hours of 9am - 5pm.
+
+I then started working in the JavaScript file to display the current date on the header element, then worked on creating click evens for each of the buttons with the corresponding hours.
+
+I then made sure the elements were saved to the localStorage after the button is clicked.
+
+I definitely ran into issues with displaying the correct color class if the hour was "past, present, or future." I will go back and figure out how to get this functioning properly.
 
 
-GIVEN I am using a daily planner to create a schedule
+## Links
 
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar 
-<!-- complete -->
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
-<!-- complete -->
-WHEN I view the time blocks for that day
-THEN each time block is color-coded to indicate whether it is in the past, present, or future
+GitHub Repo:
+https://github.com/Christianmsm/05-Daily-Planner
 
-WHEN I click into a time block
-THEN I can enter an event
+Deployed Site:
 
-WHEN I click the save button for that time block
-THEN the text for that event is saved in local storage
-<!-- completed -->
-WHEN I refresh the page
-THEN the saved events persist
+
+Screenshot:
+<img src="Develop/daily-planner-screenshot.png">
+
+## Credits
+
+W3Schools.com
+day.js.org
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Christianmsm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Features
+
+Dayjs
+Bootstrap
+JQurey
